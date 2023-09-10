3e761c4d77c967add4351003c26e459975c6891d
Ronald Almeida <ronaldmateus785@gmail.com>
2023-09-10 20:35:21 -0300

    Type Enum

---
+++ src/basicTypes/Aula09-type-enum/index.ts
@@ -0,0 +1,21 @@
+enum Cores {
+    VERMELHO = 10, // 10
+    AZUL = 100, // 100
+    AMARELO = 200, // 200
+}
+
+enum Cores {
+    ROXO = "ROXO",
+    VERDE = 201,
+    ROSA,
+}
+
+export function escolhaACor(cor: Cores): void {
+    console.log(Cores[cor]);
+}
+
+escolhaACor(201);
+escolhaACor(100);
+
+escolhaACor(Cores.VERMELHO);
+console.log(Cores[10]);
