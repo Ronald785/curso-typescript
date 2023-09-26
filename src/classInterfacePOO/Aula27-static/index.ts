96114f7fc89734ed8e68cfb0460ff15f5609f556
Ronald Almeida <ronaldmateus785@gmail.com>
2023-09-25 21:41:17 -0300

    Static

---
+++ src/classInterfacePOO/Aula27-static/index.ts
@@ -0,0 +1,33 @@
+export class Pessoa {
+    static idadePadrao = 0;
+    static cpfPadrao = "000.000.000-00";
+
+    constructor(
+        public nome: string,
+        public sobrenome: string,
+        public idade: number,
+        public cpf: string,
+    ) {}
+
+    metodoNormal(): void {
+        console.log(Pessoa.idadePadrao, Pessoa.cpfPadrao);
+    }
+
+    static criaPessoa(nome: string, sobrenome: string): Pessoa {
+        return new Pessoa(
+            nome,
+            sobrenome,
+            Pessoa.idadePadrao,
+            Pessoa.cpfPadrao,
+        );
+    }
+}
+
+const pessoa1 = new Pessoa("Ronald", "Almeida", 30, "123.456.798-00");
+const pessoa2 = Pessoa.criaPessoa("Maria", "Almeida");
+
+console.log(pessoa1);
+console.log(pessoa2);
+
+pessoa1.metodoNormal();
+console.log(Pessoa.idadePadrao, Pessoa.cpfPadrao);
