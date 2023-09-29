10dc27a3a1ecf45ab148ca4e48a6058a688d1034
Ronald Almeida <ronaldmateus785@gmail.com>
2023-09-28 21:46:06 -0300

    Interfaces

---
+++ src/classInterfacePOO/Aula34-interfaces/index.ts
@@ -0,0 +1,38 @@
+interface TipoNome {
+    nome: string;
+}
+
+interface TipoSobrenome {
+    sobrenome: string;
+}
+
+interface TipoNomeCompleto {
+    nomeCompleto(): string;
+}
+
+type TipoPessoa = TipoNome & TipoSobrenome & TipoNomeCompleto;
+interface TipoPessoa2 extends TipoNome, TipoSobrenome, TipoNomeCompleto {}
+
+export class Pessoa implements TipoPessoa2 {
+    constructor(
+        public nome: string,
+        public sobrenome: string,
+    ) {}
+
+    nomeCompleto(): string {
+        return this.nome + " " + this.sobrenome;
+    }
+}
+
+const pessoaObj: TipoPessoa2 = {
+    nome: "Ronald",
+    sobrenome: "Agora tá ok",
+    nomeCompleto() {
+        return this.nome + " " + this.sobrenome;
+    },
+};
+
+const pessoa = new Pessoa("Ronald", "Almeida");
+
+console.log(pessoa.nomeCompleto());
+console.log(pessoaObj.nomeCompleto());
