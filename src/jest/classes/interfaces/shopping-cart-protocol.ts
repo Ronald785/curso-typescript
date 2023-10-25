486c456519a2ec7aa147695bbc7be79c8a47143f
Ronald Almeida <ronaldmateus785@gmail.com>
2023-10-24 22:11:31 -0300

    Jest Order

--- src/jest/classes/interfaces/shopping-cart-protocol.ts
+++ src/jest/classes/interfaces/shopping-cart-protocol.ts
@@ -2,6 +2,7 @@ import { CartItem } from "./cart-item";
 
 export interface ShoppingCartProtocol {
     items: Readonly<CartItem[]>;
+    addItem(item: CartItem): void;
     removeItem(index: number): void;
     total(): number;
     totalWithDiscount(): number;
